6aaff40c37bcde6da10c87dee90f61133548315d
Juanflugel <juanjosegarcian1@gmail.com>
2015-10-03 18:44:24 +0200

    nivel de servicio de todo el pais en un dia

--- rutas/indicators.js
+++ rutas/indicators.js
@@ -11,34 +11,68 @@ module.exports = function indicators (app,Token,io){
 
 		var curDate = new Date(moment(new Date()).format('YYYY-MM-DD'));
 
-		Token.aggregate(
-			[
-				{ $match: { 
-					'token.state.stateCode': 3 ,
-					'token.infoToken.logEndToken':{'$gte':curDate},
-					'token.branchOffice.posCode':null 
-					}
-				},
-				{ $project: {
-					 logCreation:'$token.infoToken.logCreationToken',					 
-					 logCalled:'$token.infoToken.logCalledToken',
-					 logAtention:'$token.infoToken.logAtentionToken',
-					 logEnd:'$token.infoToken.logEndToken',
-					 totalAtention:{ $divide: [ {$subtract:['$token.infoToken.logEndToken','$token.infoToken.logCreationToken']}, 6000 ] }
-					} 
-				},
-				{ $match: { 
-					'totalAtention':{ $lte: 10}
-					}
-				},
-				// { $group: { _id:{numertor:"$token.idToken.numerator"} , total: { $sum:1 } } }
-				// { $sort: { total: -1 } }
+		var query ={
+			'token.state.stateCode': 3 ,
+			'token.infoToken.logEndToken':{'$gte':curDate},
+			'token.branchOffice.posCode':null
+		};
+
+		Token.find(query,function (err,arr){
+
+			console.log(arr.length);
+
+			Token.aggregate(
+				[	
+					{ $match: query	},
+					{ $project: {
+						logCreation:'$token.infoToken.logCreationToken',					 
+						logCalled:'$token.infoToken.logCalledToken',
+						logAtention:'$token.infoToken.logAtentionToken',
+						logEnd:'$token.infoToken.logEndToken',
+						totalAtention:{ $divide: [ {$subtract:['$token.infoToken.logEndToken','$token.infoToken.logCreationToken']}, 6000 ] }} 
+					},
+					{ $match: { 
+						'totalAtention':{ $lte: 10}
+						}
+					}				
+						// { $group: { _id:{numertor:"$token.idToken.numerator"} , total: { $sum:1 } } }
+						// { $sort: { total: -1 } }
 				],
-				function (err,obj){
-					res.json(obj);
-				});
-		
+						function (err,obj){
+							var sl = (obj.length / arr.length)*100;
+							res.json(sl);
+						}
+			);
+
+		});
+
+		// Token.aggregate(
+		// 	[	
+		// 		{ $match: { 
+		// 			'token.state.stateCode': 3 ,
+		// 			'token.infoToken.logEndToken':{'$gte':curDate},
+		// 			'token.branchOffice.posCode':null 
+		// 			}
+		// 		},
+		// 		{ $project: {
+		// 			logCreation:'$token.infoToken.logCreationToken',					 
+		// 			logCalled:'$token.infoToken.logCalledToken',
+		// 			logAtention:'$token.infoToken.logAtentionToken',
+		// 			logEnd:'$token.infoToken.logEndToken',
+		// 			totalAtention:{ $divide: [ {$subtract:['$token.infoToken.logEndToken','$token.infoToken.logCreationToken']}, 6000 ] }} 
+		// 		},
+		// 		{ $match: { 
+		// 			'totalAtention':{ $lte: 10}
+		// 			}
+		// 		},
+		// 			// { $group: { _id:{numertor:"$token.idToken.numerator"} , total: { $sum:1 } } }
+		// 			// { $sort: { total: -1 } }
+		// 	],
+		// 	function (err,obj){
+		// 			res.json(obj);
+		// 	}
+		// );
 	};
-	
+
 };
 
