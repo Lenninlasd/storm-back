2bb62685c78bc973b2fd9fa7253e7e5181e2bf0e
Juanflugel <juanjosegarcian1@gmail.com>
2015-10-03 15:21:30 +0200

    filtra los turnos segun tiempo de atencio

--- rutas/indicators.js
+++ rutas/indicators.js
@@ -9,15 +9,34 @@ module.exports = function indicators (app,Token,io){
 
 	function  getServiceLevel (req,res){
 
+		var curDate = new Date(moment(new Date()).format('YYYY-MM-DD'));
+
 		Token.aggregate(
 			[
-				{ $match: { 'token.state.stateCode': 3 } },
-				{ $group: { _id:{numertor:"$token.idToken.numerator"} , total: { $sum:1 } } }
+				{ $match: { 
+					'token.state.stateCode': 3 ,
+					'token.infoToken.logEndToken':{'$gte':curDate},
+					'token.branchOffice.posCode':null 
+					}
+				},
+				{ $project: {
+					 logCreation:'$token.infoToken.logCreationToken',					 
+					 logCalled:'$token.infoToken.logCalledToken',
+					 logAtention:'$token.infoToken.logAtentionToken',
+					 logEnd:'$token.infoToken.logEndToken',
+					 totalAtention:{ $divide: [ {$subtract:['$token.infoToken.logEndToken','$token.infoToken.logCreationToken']}, 6000 ] }
+					} 
+				},
+				{ $match: { 
+					'totalAtention':{ $lte: 10}
+					}
+				},
+				// { $group: { _id:{numertor:"$token.idToken.numerator"} , total: { $sum:1 } } }
 				// { $sort: { total: -1 } }
-			],
-			function (err,obj){
-				res.json(obj);
-		});
+				],
+				function (err,obj){
+					res.json(obj);
+				});
 		
 	};
 	
